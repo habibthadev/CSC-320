d2b0995fff7c92d5d72ca4e7e797df3821a9c18a
Habib <adebayohabib7@gmail.com>
2025-05-13 23:10:25 +0100

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,3 +1,4 @@
+/*
 import mongoose from "mongoose";
 import { config } from "dotenv" 
 
@@ -10,5 +11,40 @@ export const connectDB = async () => {
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
+  }
+};
+*/
+
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('Database successfully connected'))
+    .catch((err) => {
+      console.log(`You have this error ${err}`);
+      console.log('Retrying connection in 5 seconds...');
+      setTimeout(connectDB, 5000);
+    });
+
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+
+  process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
+
+  function gracefulExit() {
+    mongoose.connection.close(true);
   }
 };
